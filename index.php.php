eaab0f1a99f52ef4cc63bbf6c8e7e026f5d47d5f
sharobbe <sharon.rieser@gmail.com>
2019-04-04 11:34:40 +0200

    add img & bootstrap js

--- index.php.php
+++ index.php.php
@@ -1,4 +1,33 @@
+<?php
+session_start();
 
+$_SESSION["parameter1"] = 0;
+$_SESSION["parameter2"] = 0;
+$_SESSION["parameter3"] = 0;
+
+
+
+
+if(isset($_POST['login_submit'])){
+
+    $username = sql_injection_filter($_POST['username']);
+    $password = sql_injection_filter($_POST['password']);
+
+    $result = login($username , $password);
+
+    $row_count = mysqli_num_rows($result);
+
+    if($row_count == 1){
+        $user = mysqli_fetch_assoc($result);
+        $_SESSION['userid'] = $user['id'];
+        header('Location: index.php');
+    }else{
+        $msg = "Die Benutzerdaten sind nicht in unserer Datenbank vorhanden.";
+    }
+
+}
+
+?>
 <!doctype html>
 <html>
     <head>
@@ -7,9 +36,8 @@
         <meta charset="UTF-8">
         <title>To Do Liste</title>
         <!-- Bootstrap Verlinkung -->
-       
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <link rel="stylesheet" href="style/style.css">
-          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
         <style>
             .grid-container {
                 display: grid;
@@ -28,31 +56,13 @@
                 <div class="grid-item">
 
                     <div class="card border-success mb-3" style="max-width: 18rem;">
-                        <a class="btn btn-link" href="#" data-toggle="collapse" data-target="#target1">
                         <div class="card-header">Frage 1</div>
-                        </a>
-                        
-                    <div id="target1" class="collapse">
-                        
                         <div class="card-body text-success">
-                            <div class="row">
-                            <h5 class="card-title"><input type="checkbox" id="antwort1" value=1 >
-                            <label for="antwort1"><img src="img/bulbasaur.gif" width="100px"></label></h5><br>
-                            
-                                
-                            <h5 class="card-title"><input type="checkbox" id="antwort2" value=2 >
-                            <label for="antwort2"><img src="img/caterpie.gif" width="100px"></label></h5><br>
-                            </div>
-                            
-                            <div class="row">
-                            <h5 class="card-title"><input type="checkbox" id="antwort3" value=3 >
-                            <label for="antwort3"><img src="img/charmander.gif" width="100px"></label></h5><br>
-                            
-                            <h5 class="card-title"><input type="checkbox" id="antwort4" value=4 >
-                            <label for="antwort4"><img src="img/doduo.gif" width="100px"></label></h5>
-                            </div>
-                        </div>  
-                     </div>
+                            <h5 class="card-title"><input type="radio" name="parameter1" value=1 > Antwort 1</h5><br>
+                            <h5 class="card-title"><input type="radio" name="parameter1" value=4 > Antwort 2</h5><br>
+                            <h5 class="card-title"><input type="radio" name="parameter1" value=3> Antwort 3  </h5><br>
+                            <h5 class="card-title"><input type="radio" name="parameter1" value=2> Antwort 4</h5>
+                        </div>   
                     </div> 
                 </div> 
 
@@ -78,40 +88,8 @@
                             <h5 class="card-title"><input type="radio" name="parameter3" value=2> Antwort 4</h5>
                         </div>   
                     </div> 
-                    
+                    <input type="submit" name="submit" value="Get Selected Values" />
                 </div> 
-                
-                 <div class="grid-item">
-
-                    <div class="card border-success mb-3" style="max-width: 18rem;">
-                        <div class="card-header">Frage 4</div>
-                        <div class="card-body text-success">
-                            <h5 class="card-title"> sfojsfosjifoisjfosijf?<input type="range" name="frage4" min="1" max="10" >  </h5><br>
-                        </div>   
-                    </div> 
-                </div> 
-                
-                
-                
-                
-                <input type="submit" name="submit" value="Get Selected Values" />
-                
-                
-                <div class="accordion" id="accordionExample">
-
-  
-  
-</div>
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
             </form> 
         </div>
 
@@ -132,11 +110,6 @@
         ?>
 
 
-        
-          <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-  </body>
 
     </body>
 </html>
